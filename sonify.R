fa66f277d6d8e4536eda785ed90a18a3d8949ba3
Kristen Cognac <kcognac@colostate.edu>
2024-11-13 17:06:51 -0700

    Update to use PWQN data with site selection and plotting.

--- sonify.R
+++ sonify.R
@@ -3,92 +3,87 @@
 
 
 source('setup.R')
-
+light_pal <- c("#002EA3","#E70870","#745CFB","#256BF5","#1E4D2B","#56104E")
+dark_pal <- c("#000000","#FFFFFF","#1E4D2B","#745CFB","#FFCA3A","#56104E")
 
 # set wave player - Note this is for mac. 
 setWavPlayer("afplay")
 
-# Select USGS gage site_no
-site_no <- "06752260"
 
-# Download discharge
-stream_discharge <- 
-  dataRetrieval::readNWISdv(siteNumbers = site_no,
-                            parameterCd = c('00060','00065')) %>%
-  dplyr::rename_with(~c("discharge", "code"), 
-                     c("X_00060_00003", "X_00060_00003_cd")) %>%
-  mutate(date_num = as.numeric(Date),
-         log_dis = ifelse(discharge>0,log10(discharge),0))
+# Alt, get poudre sonde network data
 
-# Plot
-
-plot(stream_discharge$Date, stream_discharge$discharge, type = "l")
-
-# Select columns necessary for sound only
-sd <- stream_discharge %>%
-  dplyr::select(date_num,discharge,log_dis)
+verified_data <- load_psn()
 
 
-# Sonify using sonify package
-stream_sound <- sonify(#x = stream_discharge$date_num, 
-                       y = sd$discharge, 
-                       waveform = "square",    #c("sine", "square", "triangle","sawtooth"), 
-                       interpolation = "constant", #c("spline", "linear", "constant"),
-                       duration = 10, 
-                       flim = c(10,440), 
-                       ticks = NULL, 
-                       tick_len = 1,
-                       pulse_len = 0, 
-                       pulse_amp = 0.5, 
-                       noise_interval = c(0, 0),
-                       noise_amp = 0, 
-                       amp_level = .99, 
-                       na_freq = 300, 
-                       stereo = FALSE,
-                       smp_rate = 2000, 
-                       play = TRUE, player = NULL, player_args = NULL)
+# Select subset necessary for sound only
 
-# Will auto play with above command but can also call just play:
-play(stream_sound)
+sites <- c("archery virridy","archery","boxcreek","boxelder","cbri",              
+           "chd", "joei",  "lbea",  "legacy",  "lincoln",           
+           "pbd", "penn", "pfal", "prospect virridy", "prospect",          
+           "river bluffs", "sfm",  "springcreek", "tamasag", "timberline virridy",
+           "timberline")
 
-# Extract part or full wave
-SS1 <- extractWave(stream_sound, from=1, to=40000)
-# Extract just left channel (note above command has identical left and right but can vary...)
-SS1m <- mono(Wave(SS1), "left")
-# Calculating periodograms of sections each consisting of 64 observations,
-# overlapping by 32 observations:
-SS1_obj <- periodogram(SS1m, normalize = TRUE, width = 32, overlap = 0)
-# Plot the first periodogram:
-plot(SS1_obj, xlim = c(0, 2000), which = 1)
-# or a spectrogram
-image(SS1_obj, ylim = c(0, 1000))
-# calculate the fundamental frequency of each periodogram
-ff <- FF(SS1_obj)
-plot(ff)
-# derive note from FF given diapason a'=440
-notes <- noteFromFF(ff, 440)
-plot(notes)
-# smooth the notes:
-snotes <- smoother(notes)
-# Convert notes back to frequency
-hz_notes <- (2^(snotes/12))*440
-# Sonify new frequency (notes)
-w2 <- sonify(hz_notes, 
-             flim = c(min(hz_notes, na.rm = TRUE),max(hz_notes, na.rm = TRUE)), 
-             waveform = "triangle",    #c("sine", "square", "triangle","sawtooth"), 
-             interpolation = "constant", #c("spline", "linear", "constant"),
-             smp_rate = 8000, 
-             duration = 30)
+n <- 18
+par <- 2
+sd <- verified_data %>% 
+  dplyr::filter(site == sites[n],
+                #parameter == "FDOM Fluorescence") %>%
+                #parameter == "Specific Conductivity") %>%
+                #parameter == "ORP") %>%
+                #parameter == "pH") %>%
+                parameter == unique(parameter)[par]) %>%
+  dplyr::mutate(date = as.Date(DT_join)) %>%
+ # dplyr::filter(date < as.Date("2023-12-12")) #%>%
+  dplyr::select(c(date, parameter, value = clean_mean))
 
-# outcome should be 0 for diapason "a'" and -12 (12 halftones lower) for "a"
-plot(snotes)
-# plot melody and energy of the sound:
-melodyplot(SS1_obj, snotes)
-# apply some quantization (into 8 parts):
-qnotes <- quantize(snotes, SS1_obj@energy, parts = 64)
-# an plot it, 4 parts a bar (including expected values):
-quantplot(qnotes, expected = rep(c(0, -12), each = 2), bars = 10)
-# now prepare for LilyPond
-qlily <- quantMerge(snotes, 4, 4, 2)
+fname <- paste0(sites[n],"_",sd$parameter[par])
+
+pg <- ggplot(sd, aes(x = date, y = value)) + geom_point() + theme_bw() + 
+  labs(title = paste0("Site: ",sites[n],"\nWav file: ",fname), y = sd$parameter[par])
+
+pg
+
+ggsave(paste0("/Users/kcognac/Desktop/KEC_Docs/PSN_Sounds/",fname,"_data.jpg"),
+       plot = pg, dpi = 600, height = 3, width = 6, units = "in")  
+
+#ref_freq <- 261.63 #C
+#ref_freq <- 440 #A
+#Note	Frequency (Hz)
+#.    C4 (Middle C)	261.63
+#.    C#4/Db4	277.18
+#.    D4	293.66
+#.    D#4/Eb4	311.13
+#.    E4	329.63
+#.    F4	349.23
+#.    F#4/Gb4	369.99
+#.    G4	392.00
+#.    G#4/Ab4	415.30
+#.    A4	440.00
+#.    A#4/Bb4	466.16
+#.    B4	493.88
+
+test <- sonify_data(data_array_to_sonify = note_num_to_freq(sd$value[!is.na(sd$value)]),
+                    ref_freq = 261.63,
+                    to_plot = TRUE,
+                    raw_min = 1,
+                    raw_max = 600,
+                    total_seconds = 20,
+                    samp_width = 4*64) 
+
+
+writeWave(test[[1]], paste0("/Users/kcognac/Desktop/KEC_Docs/PSN_Sounds/",fname,".wav"))
+
+
+
+jpeg(paste0("/Users/kcognac/Desktop/KEC_Docs/PSN_Sounds/",fname,"_notes.jpg"), 
+     width = 6, height = 6, res = 200, units = "in")
+
+print(test[[2]][[6]])
+
+dev.off()
+
+
+
+
 
 
