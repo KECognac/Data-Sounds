464838ef093c63c410886ac5787f53546debe41b
Kristen Cognac <kcognac@colostate.edu>
2025-06-04 10:04:32 -0600

    function to load all psn data

--- src/load_psn.R
+++ src/load_psn.R
@@ -1,7 +1,7 @@
 load_psn <- function() {
   
   list.files("data/virridy_verification/verified_directory/", full.names = TRUE) %>%
-    map_dfr(~readRDS(.) %>% dplyr::mutate(verification_status = as.character(verification_status))) %>%
+    purrr::map_dfr(~readRDS(.) %>% dplyr::mutate(verification_status = as.character(verification_status))) %>%
     data.table::data.table() %>%
     dplyr::mutate(date = as_date(DT_round),
                   clean_mean = case_when(is.na(flag) & verification_status == "PASS" ~ mean,
